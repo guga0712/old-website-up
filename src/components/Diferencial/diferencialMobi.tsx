cb68c4d98f44ee5849408fb552b9e4fbb79ade62
Matthzao <mateusmoraes100@gmail.com>
2022-08-25 15:42:28 -0300

    added carousel

---
+++ src/components/Diferencial/diferencialMobi.tsx
@@ -0,0 +1,48 @@
+import { Flex, Text } from "@chakra-ui/react";
+import { IoMdGrid } from 'react-icons/io';
+import { TbStairsUp } from "react-icons/tb";
+import Carousel from "carousel-react-rcdev"
+import React from 'react';
+// import { Rerousel } from "rerousel";
+// import Carousel from 'nuka-carousel';
+// import { Rerousel } from 'rerousel';
+
+export default function ColaboraMobi() {
+    return (
+
+        <Flex borderTopRadius="10px" display={{ base: "block", sm: "block", md: "none", lg: "none" }} direction="column" gap="20px" alignItems="center" w="100%" h="400px" border="1px solid red" bg="black">
+            <Text marginBottom="15px" fontFamily="Raleway" fontSize="24p" textAlign="center" marginTop="20px" fontWeight="bold" color="white">Diferencial</Text>
+            <Carousel >
+                <Flex marginLeft="25px" marginRight="20px" gap="20px" direction="row">
+                    <Flex p="20px" alignItems="center" direction="column" borderRadius="6px" w="140px" h="192px" bg="#F1F1F1">
+                        <IoMdGrid size="30px" />
+                        <Text fontWeight="bold" color="black">Colaboração</Text>
+                        <Text textAlign="center" fontSize="8px">Acreditamos em uma visão de mundo colaborativa, onde nós criamos juntos. </Text>
+                    </Flex>
+                    <Flex p="20px" alignItems="center" direction="column" borderRadius="6px" w="140px" h="192px" bg="#F1F1F1">
+                        <TbStairsUp size="30px" />
+                        <Text fontWeight="bold" color="black">Colaboração</Text>
+                        <Text textAlign="center" fontSize="8px">O que nos motiva é colocar sua empresa no máximo em que ela pode chegar. </Text>
+                    </Flex>
+                    <Flex p="20px" alignItems="center" direction="column" borderRadius="6px" w="140px" h="192px" bg="#F1F1F1">
+                        <TbStairsUp size="30px" />
+                        <Text fontWeight="bold" color="black">Colaboração</Text>
+                        <Text textAlign="center" fontSize="8px">O que nos motiva é colocar sua empresa no máximo em que ela pode chegar. </Text>
+                    </Flex>
+                    <Flex p="20px" alignItems="center" direction="column" borderRadius="6px" w="140px" h="192px" bg="#F1F1F1">
+                        <TbStairsUp size="30px" />
+                        <Text fontWeight="bold" color="black">Colaboração</Text>
+                        <Text textAlign="center" fontSize="8px">O que nos motiva é colocar sua empresa no máximo em que ela pode chegar. </Text>
+                    </Flex>
+                    <Flex p="20px" alignItems="center" direction="column" borderRadius="6px" w="140px" h="192px" bg="#F1F1F1">
+                        <TbStairsUp size="30px" />
+                        <Text fontWeight="bold" color="black">Colaboração</Text>
+                        <Text textAlign="center" fontSize="8px">O que nos motiva é colocar sua empresa no máximo em que ela pode chegar. </Text>
+                    </Flex>
+                </Flex>
+            </Carousel  >
+
+        </Flex>
+        // display={{ base: "block", sm: "block", md: "block", lg: "none" }}
+    )
+}
--- src/components/Diferencial/index.tsx
+++ src/components/Diferencial/index.tsx
@@ -1,17 +1,20 @@
 import { Flex, Link, Text } from "@chakra-ui/react";
 import { FaHandsHelping } from 'react-icons/fa';
 import { TbStairsUp } from 'react-icons/tb';
-import Colabora from "./componente";
+import Colabora from "./diferencialDesk";
+import ColaboraMobi from "./diferencialMobi";
 
 
 export default function Diferencial() {
 
     return (
-
-        <Flex gap="70px" direction="column" alignItems="center" w="100%" h="600px" bg="black" justifyContent="center" borderTopRadius="10px">
-            <Text fontFamily="raleway" fontWeight="bold" fontSize="50px" color="white">Diferencial</Text>
-            <Colabora />
-            <Colabora />
-        </Flex>
+        <>
+            {/* <Flex display={{ base: "none", sm: "none", md: "none", lg: "block" }} gap="70px" direction="column" alignItems="center" w="100%" h="600px" bg="black" justifyContent="center" borderTopRadius="10px">
+                <Text textAlign="center" fontFamily="raleway" fontWeight="bold" fontSize="50px" color="white">Diferencial</Text>
+                <Colabora />
+                <Colabora />
+            </Flex> */}
+            <ColaboraMobi />
+        </>
     )
 }
